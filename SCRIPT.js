d36183233417d9f6838d7b1889db67174c9ceb32
MahmoudKhalil <arrifkak123@gmail.com>
2024-03-08 21:00:51 +0700

    update

--- SCRIPT.js
+++ SCRIPT.js
@@ -31,3 +31,29 @@ function checkHeight(){
     goTopBtn.style.display = "none"
   }
 }
+
+///search
+document.getElementById("searchButton").addEventListener("click", function() {
+  var searchTerm = document.getElementById("searchInput").value.toLowerCase(); // Convert search term to lowercase for case-insensitive comparison
+
+  // Check the search term and perform corresponding actions
+  switch(searchTerm) {
+      case "home":
+          window.location.href = "#header"; // Redirect to the home section
+          break;
+      case "about":
+          window.location.href = "#about"; // Redirect to the about section
+          break;
+      case "services":
+          window.location.href = "#services"; // Redirect to the services section
+          break;
+      case "portfolio":
+          window.location.href = "#portfolio"; // Redirect to the portfolio section
+          break;
+      case "contact":
+          window.location.href = "#contact"; // Redirect to the contact section
+          break;
+      default:
+          alert("No matching section found for the entered search term."); // Display an alert if no matching section is found
+  }
+});
